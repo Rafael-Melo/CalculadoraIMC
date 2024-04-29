c1100974be45592d13db61cfb93e49f3dfc564e3
Rafel-Melo <rafael.vilas@gmail.com>
2024-04-29 17:28:12 -0300

    Configurando banner

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
 import flet as ft
 
 def main(page:ft.Page):
-    pass
+    page.appbar = ft.AppBar(
+        leading= ft.Icon(ft.icons.MULTILINE_CHART),
+        leading_width=40,
+        title=ft.Text("Calculadora IMC", weight=ft.FontWeight.BOLD),
+        center_title=False,
+        bgcolor=ft.colors.SURFACE_VARIANT,
+    )
+
+    page.window_width = 400
+    page.window_height = 550
+
+    # Configuração do banner de notificação
+    def close_banner(e):
+        page.banner.open = False
+        page.update()
+
+
+    page.banner = ft.Banner(
+        bgcolor=ft.colors.SURFACE_VARIANT,
+        leading=ft.Icon(ft.icons.WARNING_AMBER_OUTLINED, color=ft.colors.RED, size=40),
+        content=ft.Text("Ops, preencha todos os campos!", color=ft.colors.YELLOW, weight=ft.FontWeight.BOLD),
+        actions=[
+            ft.TextButton("OK", on_click=close_banner),
+        ],
+    )
+
+
+
+    page.add()
+
+    page.update()
 
 
 ft.app(target=main)
